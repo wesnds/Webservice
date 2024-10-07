778c8b9a1c9bbde00a51f9e47719cbe9a5833883
Weslley Nascimento <wesndsantos@gmail.com>
2024-10-07 14:47:11 -0400

    service layers using best practicies. Repository -> Service -> Controller

--- src/main/java/com/webservices/webservice/controllers/UserController.java
+++ src/main/java/com/webservices/webservice/controllers/UserController.java
@@ -1,18 +1,32 @@
 package com.webservices.webservice.controllers;
 
 import com.webservices.webservice.entities.User;
+import com.webservices.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Wes", "wesnds@gmail.com", "000000000", "12345");
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> usersList = userService.getAllUsers();
+        return ResponseEntity.ok().body(usersList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 

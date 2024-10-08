12471a8ca3ba2c5f3890089120a128fd12e95b54
Weslley Nascimento <wesndsantos@gmail.com>
2024-10-08 15:33:42 -0400

    Order entity - entities relationship - using Instant over Date

---
+++ src/main/java/com/webservices/webservice/entities/Order.java
@@ -0,0 +1,71 @@
+package com.webservices.webservice.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant date;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    public Order(){}
+
+    public Order(Long id, Instant date, User customer) {
+        super();
+        this.id = id;
+        this.date = date;
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+}
--- src/main/java/com/webservices/webservice/repositories/UserRepository.java
+++ src/main/java/com/webservices/webservice/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.webservices.webservice.repositories;
 
 import com.webservices.webservice.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
